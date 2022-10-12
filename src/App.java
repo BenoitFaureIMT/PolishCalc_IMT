ec5b6c47b13010bc21625c7c2812d15896a489f7
NathanOD <nathan.odic@mines-ales.org>
2022-10-12 15:20:49 +0200

    Michael Sendivogius

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("J'aime la Pologne");
+        int i = 2;
+        while (i==2){
+            Scanner sc= new Scanner(System.in); // Création d'un système pour récupérer l'input utilisateur
+            System.out.print("Ecris un truc enculé : ");
+            String str = sc.nextLine(); // Récupération de l'input
+            System.out.println(str);
+        }
+
+        
     }
 }
