fb7ebf3994cb54a3cfdc2007ea045bc4f2c2fd84
BenoitFaureIMT <Dovahkiin2601>
2022-10-12 15:23:31 +0200

    Ludwik Wertenstein

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,28 @@
+import java.util.Stack;
+
 public class Calculator {
+
+    private Stack<Double> history = new Stack<Double>();
+    private double value = 0.0;
     
-    public void process(String input){}
+    public double process(String input){
+        try{
+            history.push(Double.parseDouble(input));
+        }catch(Exception e){
+            if(input == "+")
+                value = history.pop() + history.pop();
+            else if(input == "-")
+                value = -1 * (history.pop() - history.pop());
+            else if(input == "*")
+                value = history.pop() * history.pop();
+            else if(input == "/"){
+                double a = history.pop();
+                value = history.pop() / a;
+            }
+        }
+
+        return value;
+    }
+
+    public double getValue(){return value;}
 }
